2ba4f57f10e1675636299fc58d9cc73f0f3a45a1
tamireshc <tamireshc@gmail.com>
2024-07-23 09:57:40 -0300

    init service

--- src/main/java/com/example/microservico/controller/PropostaController.java
+++ src/main/java/com/example/microservico/controller/PropostaController.java
@@ -2,19 +2,25 @@ package com.example.microservico.controller;
 
 import com.example.microservico.dto.PropostaRequestDTO;
 import com.example.microservico.dto.PropostaResponseDTO;
+import com.example.microservico.service.PropostaService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@AllArgsConstructor
 @RestController
 @RequestMapping("/proposta")
 public class PropostaController {
+
+    private PropostaService propostaService;
+
+
     @PostMapping
     public ResponseEntity<PropostaResponseDTO> criar(@RequestBody PropostaRequestDTO requestDTO) {
-        System.out.println("ola");
-        return null;
+        PropostaResponseDTO response = propostaService.criar(requestDTO);
+        return ResponseEntity.ok(response);
     }
 }
---
+++ src/main/java/com/example/microservico/service/PropostaService.java
@@ -0,0 +1,10 @@
+package com.example.microservico.service;
+
+import com.example.microservico.dto.PropostaRequestDTO;
+import com.example.microservico.dto.PropostaResponseDTO;
+
+public class PropostaService {
+    public PropostaResponseDTO criar(PropostaRequestDTO propostaRequestDTO){
+        return null;
+    }
+}
