ace6b866405de21b06d38f38bfdd9ec5633482ed
tamireshc <tamireshc@gmail.com>
2024-08-05 18:23:09 -0300

    feat: config binding, exchanged for RabbitMQ at Propostas

--- src/main/java/com/example/microservico/config/RabbitMQConfiguration.java
+++ src/main/java/com/example/microservico/config/RabbitMQConfiguration.java
@@ -1,12 +1,35 @@
 package com.example.microservico.config;
 
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.QueueBuilder;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfiguration {
+
+    private ConnectionFactory connectionFactory;
+
+    //o próprio spring cria um @bean do connectionfactory e injeta
+    public RabbitMQConfiguration(ConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+
+    //inicializa as filas descritas no @ben no rabbitMq
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> inicializerAdmin(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+    }
+
+    @Bean
+    public RabbitAdmin criarRabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
     @Bean
     public Queue criarFilaPropostaPendenteMsAnaliseCredito() {
         return QueueBuilder.durable("proposta-pendente.ms-analise-credito").build();
@@ -25,5 +48,22 @@ public class RabbitMQConfiguration {
     @Bean
     public Queue criarFilaPropostaConcluidaMsNotificacao() {
         return QueueBuilder.durable("proposta-concluida.ms-notificacao").build();
+    }
+
+    @Bean
+    public FanoutExchange criarFanoutExchangePropostaPendente() {
+        return ExchangeBuilder.fanoutExchange("proposta-pendente.ex").build();
+    }
+
+    @Bean
+    public Binding criarBindingPropostaPendeteMsAnaliseCredito() {
+        return BindingBuilder.bind(criarFilaPropostaPendenteMsAnaliseCredito())
+          .to(criarFanoutExchangePropostaPendente());
+    }
+
+    @Bean
+    public Binding criarBindingPropostaPendenteMsNotificacao() {
+        return BindingBuilder.bind(criarFilaPropostaPendenteMsNotificacao())
+          .to(criarFanoutExchangePropostaPendente());
     }
 }
---
+++ src/main/java/com/example/microservico/service/NotificacaoService.java
@@ -0,0 +1,4 @@
+package com.example.microservico.service;
+
+public class NotificacaoService {
+}
