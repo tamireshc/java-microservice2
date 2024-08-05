6309712e5077a216c87150db71d8028674a69861
tamireshc <tamireshc@gmail.com>
2024-08-05 11:51:53 -0300

    feat: init config for queue at rabbitMQ

---
+++ src/main/java/com/example/microservico/config/RabbitMQConfiguration.java
@@ -0,0 +1,29 @@
+package com.example.microservico.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfiguration {
+    @Bean
+    public Queue criarFilaPropostaPendenteMsAnaliseCredito() {
+        return QueueBuilder.durable("proposta-pendente.ms-analise-credito").build();
+    }
+
+    @Bean
+    public Queue criarFilaPropostaPendenteMsNotificacao() {
+        return QueueBuilder.durable("proposta-pendente.ms-notificacao").build();
+    }
+
+    @Bean
+    public Queue criarFilaPropostaConcluidaMsProposta() {
+        return QueueBuilder.durable("proposta-concluida.ms-proposta").build();
+    }
+
+    @Bean
+    public Queue criarFilaPropostaConcluidaMsNotificacao() {
+        return QueueBuilder.durable("proposta-concluida.ms-notificacao").build();
+    }
+}
