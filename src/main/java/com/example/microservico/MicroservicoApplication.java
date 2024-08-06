755c9c6aaff9527507a93b517ec2238519d517e0
tamireshc <tamireshc@gmail.com>
2024-08-06 12:21:27 -0300

    feat: create method for check Propostas with Integrada = false for send again Propostas to queue of rabbitMQ

--- src/main/java/com/example/microservico/MicroservicoApplication.java
+++ src/main/java/com/example/microservico/MicroservicoApplication.java
@@ -2,8 +2,10 @@ package com.example.microservico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MicroservicoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/microservico/agendador/PropostaSemIntegracao.java
+++ src/main/java/com/example/microservico/agendador/PropostaSemIntegracao.java
@@ -4,18 +4,25 @@ import com.example.microservico.repository.PropostaRepository;
 import com.example.microservico.service.NotificacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class PropostaSemIntegracao {
 
     @Autowired
     private PropostaRepository propostaRepository;
+
     @Autowired
     private NotificacaoService notificacaoService;
+
     @Value("${rabbitmq.propostapendente.exchange}")
     private String exchange;
 
+    //agendamento para rodar o metodo a cada 10 segundos
+    @Scheduled(fixedDelay = 10, timeUnit = TimeUnit.SECONDS)
     public void buscarPropostasNaoIntegradas() {
         propostaRepository.findByIntegradaIsFalse().forEach(proposta -> {
             try {
