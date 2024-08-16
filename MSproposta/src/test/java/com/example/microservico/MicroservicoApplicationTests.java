a93463ce55cb732f9a987e30158f7a240f4f6d7d
tamireshc <tamireshc@gmail.com>
2024-08-16 11:02:07 -0300

    feat: init conf Dockerfile and docker-compose

--- MSproposta/src/test/java/com/example/microservico/MicroservicoApplicationTests.java
+++ MSproposta/src/test/java/com/example/microservico/MicroservicoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MicroservicoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
